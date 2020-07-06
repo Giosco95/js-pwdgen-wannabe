82e76e1305ef89ad0e2104da14cbe1717897fc8e
Giorgio Scollo <giosco1995@gmail.com>
2020-07-06 17:42:29 +0200

    Super password :)

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+var nome = prompt ('Come ti chiami?');
+var cognome = prompt('E il tuo cognome?');
+var colore = prompt('il tuo colore preferito');
+var superpassword = nome+cognome+colore;
+document.getElementById('ID').innerHTML= 'Ecco la tua super password' + " " +superpassword+ '19'
